0fc1279c8c5af3178e74a4fcfe84b04bd64f3cf8
Lorenzo Reboli <lorenzo.reboli.studenti@isii.it>
2020-11-23 15:50:11 +0100

    Aggiungere i file di progetto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace WA_GiocoMacchineBGW
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pct_car1 = new System.Windows.Forms.PictureBox();
+            this.pct_car2 = new System.Windows.Forms.PictureBox();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.pct_traguardo = new System.Windows.Forms.PictureBox();
+            this.txt_ngiri = new System.Windows.Forms.TextBox();
+            this.btn_Restart = new System.Windows.Forms.Button();
+            this.btn_quit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pct_car1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pct_car2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pct_traguardo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pct_car1
+            // 
+            this.pct_car1.BackColor = System.Drawing.Color.Transparent;
+            this.pct_car1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pct_car1.Image = global::WA_GiocoMacchineBGW.Properties.Resources.macchinabianca;
+            this.pct_car1.Location = new System.Drawing.Point(12, 23);
+            this.pct_car1.Name = "pct_car1";
+            this.pct_car1.Size = new System.Drawing.Size(89, 40);
+            this.pct_car1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pct_car1.TabIndex = 0;
+            this.pct_car1.TabStop = false;
+            // 
+            // pct_car2
+            // 
+            this.pct_car2.BackColor = System.Drawing.Color.Transparent;
+            this.pct_car2.Image = global::WA_GiocoMacchineBGW.Properties.Resources.macchinarossa;
+            this.pct_car2.Location = new System.Drawing.Point(12, 104);
+            this.pct_car2.Name = "pct_car2";
+            this.pct_car2.Size = new System.Drawing.Size(89, 40);
+            this.pct_car2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pct_car2.TabIndex = 1;
+            this.pct_car2.TabStop = false;
+            // 
+            // btn_start
+            // 
+            this.btn_start.BackColor = System.Drawing.Color.Transparent;
+            this.btn_start.Location = new System.Drawing.Point(613, 1);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(69, 22);
+            this.btn_start.TabIndex = 2;
+            this.btn_start.Text = "Start";
+            this.btn_start.UseVisualStyleBackColor = false;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // pct_traguardo
+            // 
+            this.pct_traguardo.BackColor = System.Drawing.Color.Transparent;
+            this.pct_traguardo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pct_traguardo.Image = global::WA_GiocoMacchineBGW.Properties.Resources.traguardo;
+            this.pct_traguardo.Location = new System.Drawing.Point(782, 1);
+            this.pct_traguardo.Name = "pct_traguardo";
+            this.pct_traguardo.Size = new System.Drawing.Size(21, 156);
+            this.pct_traguardo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pct_traguardo.TabIndex = 3;
+            this.pct_traguardo.TabStop = false;
+            // 
+            // txt_ngiri
+            // 
+            this.txt_ngiri.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_ngiri.Location = new System.Drawing.Point(688, 1);
+            this.txt_ngiri.Name = "txt_ngiri";
+            this.txt_ngiri.ReadOnly = true;
+            this.txt_ngiri.Size = new System.Drawing.Size(65, 22);
+            this.txt_ngiri.TabIndex = 4;
+            this.txt_ngiri.Text = "N.Giri:";
+            this.txt_ngiri.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btn_Restart
+            // 
+            this.btn_Restart.Location = new System.Drawing.Point(607, 1);
+            this.btn_Restart.Name = "btn_Restart";
+            this.btn_Restart.Size = new System.Drawing.Size(75, 23);
+            this.btn_Restart.TabIndex = 5;
+            this.btn_Restart.Text = "Restart";
+            this.btn_Restart.UseVisualStyleBackColor = true;
+            this.btn_Restart.Visible = false;
+            this.btn_Restart.Click += new System.EventHandler(this.btn_Restart_Click);
+            // 
+            // btn_quit
+            // 
+            this.btn_quit.Location = new System.Drawing.Point(-5, -6);
+            this.btn_quit.Name = "btn_quit";
+            this.btn_quit.Size = new System.Drawing.Size(45, 23);
+            this.btn_quit.TabIndex = 6;
+            this.btn_quit.Text = "quit";
+            this.btn_quit.UseVisualStyleBackColor = true;
+            this.btn_quit.Click += new System.EventHandler(this.btn_quit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WA_GiocoMacchineBGW.Properties.Resources.road_trip_background_10864211;
+            this.ClientSize = new System.Drawing.Size(800, 156);
+            this.Controls.Add(this.btn_quit);
+            this.Controls.Add(this.btn_Restart);
+            this.Controls.Add(this.txt_ngiri);
+            this.Controls.Add(this.pct_traguardo);
+            this.Controls.Add(this.btn_start);
+            this.Controls.Add(this.pct_car2);
+            this.Controls.Add(this.pct_car1);
+            this.MaximumSize = new System.Drawing.Size(816, 195);
+            this.MinimumSize = new System.Drawing.Size(816, 195);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pct_car1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pct_car2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pct_traguardo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pct_car1;
+        private System.Windows.Forms.PictureBox pct_car2;
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.PictureBox pct_traguardo;
+        private System.Windows.Forms.TextBox txt_ngiri;
+        private System.Windows.Forms.Button btn_Restart;
+        private System.Windows.Forms.Button btn_quit;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WA_GiocoMacchineBGW
+{
+    public partial class Form1 : Form
+    {
+        //variabili global
+        BackgroundWorker bgw1, bgw2;
+        bool vittoria = false;
+        int v1, v2, ngiri,ntrag1, ntrag2, ntrag;
+        bool ripeti = false;
+
+        private void btn_quit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_Restart_Click(object sender, EventArgs e)
+        {
+           //faccio ripartire tutto
+            vittoria = false;
+            v1 =0;
+            v2 = 0;
+            ngiri = 0;
+            ntrag = 0;
+            ntrag1 = 0;
+            ntrag2 = 0;
+            ripeti = false;
+            btn_start.Enabled = true;
+            btn_start.Visible = true;
+            btn_Restart.Visible = false;
+            pct_car1.Location = new Point(12, 23);
+            pct_car2.Location = new Point(12, 104);
+            txt_ngiri.Text = "N.Giri:" + "";
+            btn_quit.Enabled = true;
+        }
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            //disabilito lo start e lo rendo invisibile
+            btn_start.Enabled = false;
+            btn_start.Visible = false;
+            btn_quit.Enabled = false;
+
+            //estraggo il numero di giri
+            if (ripeti == false)
+            {
+                //numero random per i giri
+                 Random rnd = new Random();
+                 ngiri = rnd.Next(1, 11);
+                 txt_ngiri.Text = "N.Giri:"+ngiri.ToString();
+
+                //assegno alle variabili "check" il numero di giri da fare                
+                ntrag = ngiri;
+                ntrag1 = ngiri;
+                ntrag2 = ngiri;
+                ripeti = true;
+            }
+
+            //creo un backgroundworker per la macchina1
+            bgw1 = new BackgroundWorker();
+            bgw1.WorkerReportsProgress = true;
+            bgw1.WorkerSupportsCancellation = true;
+            bgw1.DoWork += Bgw_DoWork;
+            bgw1.ProgressChanged += Bgw_ProgressChanged;
+            bgw1.RunWorkerCompleted += Bgw_RunWorkerCompleted;
+
+            //creo un backgroundworker per la macchina2
+            bgw2 = new BackgroundWorker();
+            bgw2.WorkerReportsProgress = true;
+            bgw2.WorkerSupportsCancellation = true;
+            bgw2.DoWork += Bgw_DoWork;
+            bgw2.ProgressChanged += Bgw_ProgressChanged;
+            bgw2.RunWorkerCompleted += Bgw_RunWorkerCompleted;
+
+            //avvio i backgroundworker
+            bgw1.RunWorkerAsync();
+            bgw2.RunWorkerAsync();
+
+        }
+       
+        private void Bgw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            
+        }
+
+        private void Bgw_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            //faccio muovere la macchina1 e la macchina2          
+            pct_car1.Left = pct_car1.Left + v1;
+            pct_car2.Left = pct_car2.Left + v2;
+
+            //ciclo per far fare tutti i giri
+            for (int i = 0; i <= ngiri; i++)
+            {
+                if (vittoria == false)
+                {
+                    //controllo se il check sui giri fatti è 0, se si dichiaro chi ha vinto
+                    if (ntrag1 == 0)
+                    {
+                        vittoria = true;
+                        MessageBox.Show("Macchina 1 ha Vinto");
+                        btn_Restart.Visible = true;
+                        btn_quit.Enabled = true;                        
+                    }
+                    if (ntrag2 == 0)
+                    {
+                        vittoria = true;
+                        MessageBox.Show("Macchina 2 ha Vinto");
+                        btn_Restart.Visible = true;
+                        btn_quit.Enabled = true;                       
+                    }
+
+                    //finché i giri sono diversi da 0 faccio andare le macchine avanti
+                    if (ntrag !=0)
+                    {
+                        if (pct_car1.Bounds.IntersectsWith(pct_traguardo.Bounds))
+                        {
+                            ntrag1--;
+                            pct_car1.Location = new Point(12, 23);
+                        }
+                        if (pct_car2.Bounds.IntersectsWith(pct_traguardo.Bounds))
+                        {
+                            ntrag2--;
+                            pct_car2.Location = new Point(12, 104);
+                        }
+                    }                    
+                }
+
+            }
+        }
+
+        private void Bgw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker bgw = sender as BackgroundWorker;
+            while (vittoria != true)
+            {
+                //fino a quando la booleana non è true estraggo numeri random che assegno a due variabili per far muovere di n le macchine
+                Random rnd = new Random();
+                Thread.Sleep(100);
+                v1 = rnd.Next(1, 11);
+                v2 = rnd.Next(1, 11);
+
+                //passo grazie alla ReportProgress le due velocità random
+                bgw.ReportProgress(v1, v2);
+            }            
+        }
+    }
+}
